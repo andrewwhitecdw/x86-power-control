28425c96d21b805828d7f325e4e030b23f9af206
andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>
2026-08-13 21:32:22 -0500

    fix: log claims BMC SSD Reset is asserted when it is not
    Signed-off-by: andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>

--- src/platform/nvl144/nvl144_power_control.cpp
+++ src/platform/nvl144/nvl144_power_control.cpp
@@ -772,7 +772,7 @@ void NVL144PowerControl::deassertHPMPowerAndPeripherals()
                       !board1RunPowerEnable->second->polarity);
     }
 
-    // De-assert peripheral power and assert BMC SSD Reset and SSD Powe
+    // De-assert peripheral power (USB and E1S)
     setGPIOOutput(usbPowerEnable->second, !usbPowerEnable->second->polarity);
     setGPIOOutput(e1sPowerEnable->second, !e1sPowerEnable->second->polarity);
 }
@@ -783,7 +783,7 @@ void NVL144PowerControl::transitionToHPMPowerGoodDeAssertState()
     cancelTimer("CPU Reset Watchdog Timer", cpuResetWatchdogTimer);
 
     lg2::info(
-        "CPU Reset Indicator Asserted. CPUs are in reset. De-asserting Run Power Enable, E1S Power Enable, USB Power Enable, and asserting BMC SSD Reset lines. Starting HPM Power Good Watchdog Timer. Transitioning to PowerState::waitForHPMPowerGoodDeAssert.");
+        "CPU Reset Indicator Asserted. CPUs are in reset. De-asserting Run Power Enable, E1S Power Enable, and USB Power Enable. Starting HPM Power Good Watchdog Timer. Transitioning to PowerState::waitForHPMPowerGoodDeAssert.");
 
     deassertHPMPowerAndPeripherals();
     startTimer("HPMPowerGoodWatchdogMs", hpmPowerGoodWatchdogTimer,
