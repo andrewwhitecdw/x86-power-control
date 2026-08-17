16c23b8d295e8c8f65f576fdb10c705a540370aa
andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>
2026-08-17 03:46:41 -0500

    fix: Remove implementation-only includes from bmc_state_manager.hpp
    Signed-off-by: andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>

--- src/bmc/bmc_state_manager.hpp
+++ src/bmc/bmc_state_manager.hpp
@@ -6,12 +6,9 @@
 #include "utils.hpp"
 #include "xyz/openbmc_project/State/BMC/server.hpp"
 
-#include <linux/watchdog.h>
-#include <sys/sysinfo.h>
 
 #include <sdbusplus/bus.hpp>
 
-#include <cassert>
 #include <chrono>
 #include <string>
 #include <unordered_map>
